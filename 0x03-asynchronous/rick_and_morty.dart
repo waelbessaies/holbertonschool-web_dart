730caddb011fbd9f4900449a6043d6340b0b4f33
waelbessaies <waelbessaies2@gmail.com>
2024-05-26 10:53:38 +0100

    fix

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,19 +1,17 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<void> printRmCharacters() async {
-  try {
-    final response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final characters = data['results'] as List;
-      characters.forEach((character) {
-        print(character['name']);
-      });
-    } else {
-      print('Failed to load characters');
+printRmCharacters() async {
+  final response =
+      await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+
+  if (response.statusCode == 200) {
+    var data = jsonDecode(response.body);
+    List characters = data['results'];
+    for (var character in characters) {
+      print(character['name']);
     }
-  } catch (e) {
-    print('error caught: $e');
+  } else {
+    throw Exception('Failed to load album');
   }
 }
